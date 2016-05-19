809cdd4ee99e5ddc32086dd648646fff1f768ec4
Michael Bebenita <mbebenita@gmail.com>
2016-05-18 19:33:09 -0700

    Update.

--- explorer.js
+++ explorer.js
@@ -314,6 +314,21 @@ var cppExamples = {
     n *= i;
   }
   return (double)n;
+}`,
+  "virtual": `struct A {
+  A();
+  ~A();
+  virtual void virtual_member_function();
+};
+ 
+A *ctor() {
+  return new A();
+}
+void dtor(A *a) {
+  delete a;
+}
+void call_member_function(A *a) {
+  a->virtual_member_function();
 }`
 }
 
