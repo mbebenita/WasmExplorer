e16ddf5c16b3fa96170515e57ba61fdc8d747ac9
Michael Bebenita <mbebenita@gmail.com>
2016-05-20 16:18:14 -0700

    Fixes.

--- explorer.js
+++ explorer.js
@@ -471,6 +471,27 @@ double do_pow(double x) {
 double factor(double a, double b, double c) {
   return (a * c) + (b * c);
 }
+`, "duff": `/**
+  More expressive control flow constructs are needed to 
+  implement Duff's device effectively.
+  See: 
+  https://github.com/WebAssembly/design/blob/master/FutureFeatures.md#more-expressive-control-flow
+  */
+void send(char *to, char *from, unsigned long count)
+{
+  unsigned long n = (count + 7) / 8;
+  switch (count % 8) {
+  case 0: do { *to++ = *from++;
+  case 7:      *to++ = *from++;
+  case 6:      *to++ = *from++;
+  case 5:      *to++ = *from++;
+  case 4:      *to++ = *from++;
+  case 3:      *to++ = *from++;
+  case 2:      *to++ = *from++;
+  case 1:      *to++ = *from++;
+    } while (--n > 0);
+  }
+}
 `
 }
 
