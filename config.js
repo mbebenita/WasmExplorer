0296e57c042c20bdc3d0fdc9bb27d1f8a5b71411
Yury Delendik <ydelendik@mozilla.com>
2017-03-08 13:25:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var WasmExplorerVersion = "v2.12";
+var WasmExplorerVersion = "v2.13";
 
 var WasmExplorerServiceBaseUrl = "https://wasmexplorer-service.herokuapp.com/";
 
